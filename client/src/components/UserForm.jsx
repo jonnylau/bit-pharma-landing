fb8b1440dc29a1654ecee90016b23e940ceff905
Jon Lau <jonlau@Jons-MacBook-Air.local>
2018-01-20 21:51:12 -0800

    Forms capturing data on Change

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
 
-const UserForm = (props) => {
-  return (
-  <div>
-    <p>This is the User Form</p>
-  </div>
-  )
-}
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      practitioner: false,
+      specialty: '',
+    }
+
+    this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+    this.handleChangeLastName = this.handleChangeLastName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePhysician = this.handleChangePhysician.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+  
+  handleChangeFirstName(event) {
+    this.setState({ firstName: event.target.value});
+    console.log(event.target.value);
+  }
+  handleChangeLastName(event) {
+    this.setState({ lastName: event.target.value });
+    console.log(event.target.value);
+  }
+  handleChangeEmail(event) {
+    this.setState({ email: event.target.value })
+    console.log(event.target.value);
+  }
+  handleChangePhysician(event) {
+    this.setState({ practitioner: !this.state.practitioner });
+    console.log(this.state.practitioner);
+  }
+  submitForm(event){
+    console.log(this.state);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            First Name
+            <input type='text' value={this.state.firstName} onChange={this.handleChangeFirstName}/>
+          </label>
+          <label>
+            Last Name
+            <input type='text' value={this.state.lastName} onChange={this.handleChangeLastName} />
+          </label>
+          <label>
+            Email
+            <input type='text' value={this.state.email} onChange={this.handleChangeEmail} />
+          </label>
+          <label>
+            Are you a physician?
+            <input type='checkbox' onChange={this.handleChangePhysician} />
+          </label>
+          <input type='button' value="Submit Info" onClick={this.submitForm}/>
+        </form>
+      </div>
+    )
+  }
+};
 
 export default UserForm;
